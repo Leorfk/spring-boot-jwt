cecc1917ee8d38d995a23bc199e94c599ea28270
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-03-30 21:51:14 -0300

    Adição do mascote

--- src/main/java/com/projetospring/apijava/ApijavaApplication.java
+++ src/main/java/com/projetospring/apijava/ApijavaApplication.java
@@ -8,6 +8,11 @@ public class ApijavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApijavaApplication.class, args);
+		System.out.println("\nWelcome " +
+				"\n  _____" +
+				"\n  |0V0|" +
+				"\n /| O |\\" +
+				"\n  |_|_|");
 	}
 
 }
