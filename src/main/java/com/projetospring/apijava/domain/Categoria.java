39993aa5356eb1b4c59dd67cbb892edab9b58676
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-04-01 00:26:05 -0300

    Testando a conexão com o banco de dados H2 e criação da tabela categoria

--- src/main/java/com/projetospring/apijava/domain/Categoria.java
+++ src/main/java/com/projetospring/apijava/domain/Categoria.java
@@ -2,12 +2,19 @@ package com.projetospring.apijava.domain;
 
 import com.projetospring.apijava.resource.CategoriaResource;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
     private static long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
