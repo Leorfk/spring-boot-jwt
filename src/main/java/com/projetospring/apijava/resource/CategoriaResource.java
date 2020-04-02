1198db3c5930d7bcd1778d5f763c20b6153c89c7
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-04-01 21:40:03 -0300

    Criação da repository e service de categoria

--- src/main/java/com/projetospring/apijava/resource/CategoriaResource.java
+++ src/main/java/com/projetospring/apijava/resource/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.projetospring.apijava.resource;
 
+import com.projetospring.apijava.domain.Categoria;
+import com.projetospring.apijava.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -10,4 +15,12 @@ import java.util.Objects;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public ResponseEntity<Categoria> findById(@RequestParam(value="id" ) Integer id){
+        Categoria cat = categoriaService.buscar(id);
+        return cat != null ? ResponseEntity.ok(cat) : ResponseEntity.noContent().build();
+    }
 }
