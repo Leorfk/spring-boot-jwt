355165bd0de9c89377a12559e9fd1a81ae068fb5
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-04-11 23:03:29 -0300

    Criação da validação de email já cadastrado

--- src/main/java/com/projetospring/apijava/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/projetospring/apijava/service/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.projetospring.apijava.service.validation;
 
+import com.projetospring.apijava.domain.Cliente;
 import com.projetospring.apijava.domain.enums.TipoCliente;
 import com.projetospring.apijava.dto.ClienteNewDTO;
+import com.projetospring.apijava.repository.ClienteRepository;
 import com.projetospring.apijava.resource.exception.FieldMessage;
 import com.projetospring.apijava.service.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,9 @@ import javax.validation.ConstraintValidatorContext;
 
 // ConstraintValidator<NomeDaAnotacao, TipoDaAnotacao>
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -25,11 +31,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfCnpj())) {
             list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
         }
-
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
             list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
         }
-
+        Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+        if (cliente != null){
+            list.add(new FieldMessage("email", "Email já cadastrado"));
+        }
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getName())
