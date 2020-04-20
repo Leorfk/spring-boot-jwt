ea0de762a33e2d922c6be984720b29e6448628e9
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-04-19 23:28:54 -0300

    corrigindo erro ao realizar uma requisição com valor de categoria nula

--- src/main/java/com/projetospring/apijava/resource/ProdutoResource.java
+++ src/main/java/com/projetospring/apijava/resource/ProdutoResource.java
@@ -33,7 +33,7 @@ public class ProdutoResource {
     @GetMapping(value = "/paginas")
     public ResponseEntity<Page<ProdutoDTO>> findPage(
             @RequestParam(value="nome", defaultValue="") String nome,
-            @RequestParam(value="categorias", defaultValue="") String categorias,
+            @RequestParam(value="categorias", defaultValue="0") String categorias,
             @RequestParam(value="page", defaultValue="0") Integer page,
             @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
             @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
