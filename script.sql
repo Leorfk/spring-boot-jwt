dd6c0d2cffd0c43e5991e9e21a540b7279668399
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-05-03 20:45:49 -0300

    Realizando backup da base de dados

--- script.sql
+++ script.sql
@@ -2,3 +2,5 @@ CREATE DATABASE curso_spring;
 CREATE USER 'robot'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON * . * TO 'robot'@'localhost';
 FLUSH PRIVILEGES;
+-- comando para gerar backup do banco de dados local
+mysqldump -u <user> -p <dbname> > arquivo.sql
