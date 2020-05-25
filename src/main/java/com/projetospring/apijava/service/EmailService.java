785b0786e45f3fe68b1272a335a99ffdf45c6b00
leonardo <leonardorodrigues_f@hotmail.com>
2020-05-25 17:00:00 -0300

    esqueci a senha

--- src/main/java/com/projetospring/apijava/service/EmailService.java
+++ src/main/java/com/projetospring/apijava/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.projetospring.apijava.service;
 
+import com.projetospring.apijava.domain.Cliente;
 import com.projetospring.apijava.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -7,4 +8,5 @@ public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendNewPasswordEmail(Cliente cliente, String newPassword);
 }
